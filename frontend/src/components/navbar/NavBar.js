fdb8f5313b7d94b80b0e22a582601e81b5812432
Gael González <gaeldev032@gmail.com>
2023-07-31 13:50:10 -0700

    Adding navbar and installing react-bootstrap

---
+++ frontend/src/components/navbar/NavBar.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes.js";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export const NavigationBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-dark navbar-dark">
+      <Container fluid>
+        <Navbar.Brand as={Link} to={ROUTES.HOME}>
+          MelonMix
+        </Navbar.Brand>
+        {/* //? Enable to add search input
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll></Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse> */}
+      </Container>
+    </Navbar>
+  );
+};
--- frontend/src/layouts/HomeLayout.js
+++ frontend/src/layouts/HomeLayout.js
@@ -1,6 +1,9 @@
+import { NavigationBar } from "../components/navbar/NavBar";
+
 export const HomeLayout = (props) => {
   return (
     <div id="app__home-layout">
+      <NavigationBar />
       <>{props.children}</>
     </div>
   );
